01a9612b6e8eae87965dd40ed368dd9440e43f59
Alex Perathoner <alex.perathoner@gmail.com>
2022-12-28 22:18:43 +0100

    update path

--- Configuration/generate_latest_changes.py
+++ Configuration/generate_latest_changes.py
@@ -19,10 +19,11 @@ def get_latest_release_notes():
 
 if __name__ == '__main__':
     title, latest_version, notes = get_latest_release_notes()
+    print(title, latest_version, notes, sep='\n')
     # write each value to a file
-    with open('../new_version', 'w') as f:
+    with open('new_version', 'w') as f:
         f.write(latest_version)
-    with open('../latest_changes', 'w') as f:
+    with open('latest_changes', 'w') as f:
         f.write(notes)
-    with open('../title', 'w') as f:
+    with open('title', 'w') as f:
         f.write(title)
