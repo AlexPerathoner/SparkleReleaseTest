ab0897cf7000e80bbc8481b0132fb30139d79ae7
Alex Perathoner <alex.perathoner@gmail.com>
2022-12-28 14:30:53 +0100

    add proj

---
+++ testSparkleRelease/testSparkleRelease/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testSparkleRelease
+//
+//  Created by Alex Perathoner on 28/12/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
