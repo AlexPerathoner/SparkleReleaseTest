0d7aa989748a63e73affddd42e8c1ff48f387d94
Alex Perathoner <alex.perathoner@gmail.com>
2023-01-08 22:07:26 +0100

    add beta updates checkbox

--- testSparkleRelease/ViewController.swift
+++ testSparkleRelease/ViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import Cocoa
+import Sparkle
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, SPUUpdaterDelegate {
+    
+    @IBOutlet weak var checkBetaUpdatesOutlet: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +21,14 @@ class ViewController: NSViewController {
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
+        }
+    }
+    
+    func allowedChannels(for updater: SPUUpdater) -> Set<String> {
+        if(checkBetaUpdatesOutlet.state == .on) {
+            return Set(["beta"])
+        } else {
+            return Set()
         }
     }
 
